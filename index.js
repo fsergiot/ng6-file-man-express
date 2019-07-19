6bf046e3f4fee235349d90d9088851f3279c10a9
fsergiot <sergio.fernandez@tictapps.com>
2019-07-19 12:41:56 -0300

    habilitacion de cors

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const defaultPath = settings.defaultFolder;
 
 const app = express();
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.listen(process.env.PORT || settings.serverPort, () => {
   console.log('Server running on port ' + process.env.PORT || settings.serverPort);
 });
